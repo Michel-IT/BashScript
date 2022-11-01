0ef4a2d1d76d149b948fee2a524198c3ab9d2ef2
Michel-IT <39780488+Michel-IT@users.noreply.github.com>
2022-11-01 12:00:38 +0100

    Create FritzBox_Reboot.sh

---
+++ FRITZBox/FritzBox_Reboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IP="192.168.178.1"
+FRITZUSER="fritz1234"
+FRITZPW="password1234"
+location="/upnp/control/deviceconfig"
+uri="urn:dslforum-org:service:DeviceConfig:1"
+action='Reboot'
+curl -k -m 5 --anyauth -u "$FRITZUSER:$FRITZPW" http://$IP:49000$location -H 'Content-Type: text/xml; charset="utf-8"' -H "SoapAction:$uri#$action" -d "<?xml version='1.0' encoding='utf-8'?><s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'><s:Body><u:$action xmlns:u='$uri'></u:$action></s:Body></s:Envelope>" -s > /dev/null
